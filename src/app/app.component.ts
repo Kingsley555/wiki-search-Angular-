995c3b80aabb3b143256026f17e064e5b90dd8ce
Kingsley Oha <KingsleyOha@Names-MacBook-Air.local>
2020-08-19 16:40:42 +0200

    search-component updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { WikipediaService } from './wikipedia.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'search';
+  constructor(private wikipedia: WikipediaService) {}
+
+  onTerm(term: string) {
+    const results = this.wikipedia.search(term);
+    console.log(results);
+  }
 }
+
+//https://en.wikipedia.org/w/api.php?
+//action=query&format=json&list=search&utf8=1&srsearch=space
